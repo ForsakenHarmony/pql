7a6cc2fb83eb9e1c0db6f249a5e9f064ff1e13d9
Leah <me@hrmny.sh>
2019-02-13 01:58:42 +0100

    feat: hooks that are myb not AS bad

--- @pql/hooks/src/index.ts
+++ @pql/hooks/src/index.ts
@@ -1,11 +1,21 @@
 import { Client } from '@pql/client';
 import { createContext } from 'preact';
-import { useContext, useState, useEffect } from 'preact/hooks';
+import { useContext, useState, useEffect, useCallback } from 'preact/hooks';
 import { CtxFactory } from '@pql/client';
+import { IOperation } from '@pql/preact';
 
 export const PQLContext = createContext<Client | null>(null);
 export const Provider = PQLContext.Provider;
 export const Consumer = PQLContext.Consumer;
+
+export function query<Vars>(
+  query: CtxFactory<Vars>,
+  variables?: Vars
+): IOperation<Vars> {
+  return { query, variables };
+}
+export const mutation = query;
+export const subscription = query;
 
 // assign<T, U>(target: T, source: U): T & U;
 // Lighter Object.assign stand-in
@@ -26,9 +36,28 @@ interface QueryState {
   loading: boolean;
 }
 
+function useInvalidation<Vars>(queries: IOperation<Vars>[], cb: Function) {
+  const client = useClient();
+  const hashes = queries.map(({ query, variables }) =>
+    Client.hash(query.query, variables)
+  );
+
+  useEffect(() => {
+    let cleanup = cb();
+    const unsub = client.onInvalidate(
+      hash =>
+        hashes.includes(hash) &&
+        (typeof cleanup === 'function' && cleanup(), cb())
+    );
+    return () => {
+      unsub();
+      typeof cleanup === 'function' && cleanup();
+    };
+  }, hashes);
+}
+
 export function useQuery<T extends {}, Vars>(
-  query: CtxFactory<Vars>,
-  variables?: Vars
+  query: IOperation<Vars>
 ): QueryState & T {
   const client = useClient();
 
@@ -36,13 +65,10 @@ export function useQuery<T extends {}, Vars>(
   const [error, setError] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
+  useInvalidation([query], () => {
     setLoading(true);
     client
-      .query<T, Vars>({
-        query,
-        variables,
-      })
+      .query<T, Vars>(query)
       .then(({ data, error }) => {
         setData(data);
         setError(error);
@@ -52,7 +78,7 @@ export function useQuery<T extends {}, Vars>(
         setError(error);
         setLoading(false);
       });
-  }, []);
+  });
 
   return assign(
     {
@@ -71,7 +97,7 @@ interface MutationState<T, Vars> {
 }
 
 export function useMutation<T, Vars>(
-  mutation: CtxFactory<Vars>
+  mutation: IOperation<Vars>
 ): MutationState<T, Vars> {
   const client = useClient();
 
@@ -81,9 +107,9 @@ export function useMutation<T, Vars>(
 
   function mutate(variables: Vars) {
     setLoading(true);
-    client
+    return client
       .query<T, Vars>({
-        query: mutation,
+        query: mutation.query,
         variables,
       })
       .then(({ data, error }) => {
@@ -102,5 +128,80 @@ export function useMutation<T, Vars>(
     error,
     loading,
     mutate,
+  };
+}
+
+interface SubscriptionOpts<QData, SData, OData, QVars, SVars> {
+  query: IOperation<QVars>;
+  subscription: IOperation<SVars>;
+  updateData: (data: QData, next?: SData) => OData;
+}
+
+interface SubscriptionState<T> {
+  data: T | null;
+  error?: any;
+  loading: boolean;
+  stopped: boolean;
+  stop: () => void;
+}
+
+export function useSubscription<QData, SData, OData, QVars, SVars>({
+  query,
+  subscription,
+  updateData,
+}: SubscriptionOpts<QData, SData, OData, QVars, SVars>): SubscriptionState<
+  OData
+> {
+  const client = useClient();
+
+  const [data, setData] = useState<OData | null>(null);
+  const [error, setError] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [stopped, setStopped] = useState<boolean>(false);
+
+  let stopsub = () => {};
+
+  const stop = useCallback(() => {
+    setStopped(true);
+    stopsub();
+  }, []);
+
+  useInvalidation([query], () => {
+    setLoading(true);
+    (async () => {
+      try {
+        const { data, error } = await client.query<QData, QVars>(query);
+        if (stopped) return;
+        data && setData(updateData(data));
+        error && setError(error);
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+        return setError(err);
+      }
+      if (stopped) return;
+      try {
+        await client
+          .subscribe<SData, SVars>(subscription)
+          .forEach((res, cancel) => {
+            stopsub = cancel;
+            res.error && setError(res.error);
+          });
+      } catch (err) {
+        setStopped(true);
+        setError(err);
+      }
+    })();
+    return () => {
+      stop();
+    };
+  });
+
+  return {
+    data,
+    error,
+    loading,
+    stopped,
+    stop,
   };
 }
